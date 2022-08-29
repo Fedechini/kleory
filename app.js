6537f556681c59c09c0f60ad39042c6cd327d2a0
Fede Bertoni <mitrero1@gmail.com>
2022-08-29 20:08:28 +1000

    config

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(morgan('dev'));
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ROUTES
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
