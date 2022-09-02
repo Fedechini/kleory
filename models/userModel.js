188dfa03f4e0565cc0b100e8c2e083d12cde74f9
Fede Bertoni <mitrero1@gmail.com>
2022-09-02 15:32:06 +1000

    signup users

---
+++ models/userModel.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'User must have a name'],
+    maxlength: [20, 'Username must not be longer than 20 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // this validator only works on CREATE() and SAVE()
+      validator: function (passwordConfirm) {
+        return passwordConfirm === this.password;
+      },
+      message: 'Passwords do not match',
+    },
+  },
+});
+
+// hash password if is been modified
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // delete passwordConfirm field, only needed for validation
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
